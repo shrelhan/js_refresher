89f361608ff6ccbfaab2f0dac964f84be354b3ef
Shubham Relhan <shrelhan.relhan@gmail.com>
2024-03-26 20:16:33 +0000

    dataTypes

---
+++ 1_basics/2_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all JS as latest version
+
+let name = "sr";
+let age = 18;
+let isLoggedIn = true;
+
+// Data Types: number, string, bigint, boolean, null(standalone value), undefined(unassigned variable), symbol
+//Data Types: Object
+
+console.log(typeof age); // to check type of any value or var
