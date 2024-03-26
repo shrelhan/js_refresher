fcb6197ca07ec4018b078a10d0158edd40ea75d4
Shubham Relhan <shrelhan.relhan@gmail.com>
2024-03-26 20:53:14 +0000

    conversion done

--- 1_basics/3_conversions.js
+++ 1_basics/3_conversions.js
@@ -22,3 +22,13 @@
 // 1: true. 0: false
 // "": false
 // "hi": true
+
+// ********Operations*********
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+let str1 = "hello";
+let str2 = " sr";
+let str3 = str1 + str2;
