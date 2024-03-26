b81ae2468e8e7a07e9b562ce54413c6fa71eb9a4
Shubham Relhan <shrelhan.relhan@gmail.com>
2024-03-26 20:30:12 +0000

    coversion of dataTypes

---
+++ 1_basics/3_conversions.js
@@ -0,0 +1,24 @@
+// let score = true;
+
+// console.log(typeof score);
+
+// let scoreInNumber = Number(score);
+
+// console.log(typeof scoreInNumber);
+// console.log(scoreInNumber);
+
+// to number conversion using Number()
+// "33": 33
+// "33aa": NaN
+// true: 1, false: 0
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// to boolean conversion using Boolean()
+// 1: true. 0: false
+// "": false
+// "hi": true
