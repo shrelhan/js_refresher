72758f9ba8e6a8dde0232979f6c857234079df54
Shubham Relhan <shrelhan.relhan@gmail.com>
2024-03-26 21:28:54 +0000

    dataType summary

---
+++ 1_basics/dataTypes_summary.js
@@ -0,0 +1,7 @@
+// Primitive, call by value
+    // 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Non-primitive or Reference, call by reference
+    // Array, Objects, Functions
+
+
