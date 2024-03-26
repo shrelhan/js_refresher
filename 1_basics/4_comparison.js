e2592edf6fb4c40b770db71e8887ba7f59d3ef04
Shubham Relhan <shrelhan.relhan@gmail.com>
2024-03-26 21:08:19 +0000

    comparisons done

---
+++ 1_basics/4_comparison.js
@@ -0,0 +1,7 @@
+// console.log(2 > 3);
+// console.log(2 < 3); 
+// console.log(2 >= 3);
+// console.log(2 <= 3);
+// console.log(2 == 3); equality check, doesn't check data types
+// console.log(2 === 3); strict equality, check operands as well as data types
+// console.log(2 != 3);
