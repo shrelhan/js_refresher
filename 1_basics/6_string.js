e8e464bece0ba385eeea6b2ae0ce0c40be221e6d
Shubham Relhan <shrelhan.relhan@gmail.com>
2024-03-27 20:15:26 +0000

    number and string done

---
+++ 1_basics/6_string.js
@@ -0,0 +1,18 @@
+let firstName = "S";
+let lastName = " R";
+let fullNme = firstName + lastName;
+
+// console.log(fullNme);
+
+// console.log(`Hello my name is ${fullNme}`);
+
+let name = new String('google . com / sr %20 rs');
+
+// console.log(name);
+// console.log(name.toLowerCase());
+// console.log(name.charAt(1));
+// console.log(name.indexOf('R'));
+// console.log(name.substring(2));
+// console.log(name.trim());
+// console.log(name.replace('%20', '-'));
+// console.log(name.split(' '));
---
+++ 1_basics/7_number.js
@@ -0,0 +1,16 @@
+const creditScore = 800;
+const balance = 80000;
+
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+// console.log(balance.toLocaleString('en-IN'));
+
+// --------------------Maths----------------------
+
+// console.log(Math.abs(-55627));
+// console.log(Math.floor(4.6));
+// console.log(Math.sqrt(25));
+
+// console.log((Math.random()*10) + 1);
+
