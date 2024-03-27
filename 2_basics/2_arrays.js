1a5585641bbfe6b29412c2274872ba26d52f8d82
Shubham Relhan <shrelhan.relhan@gmail.com>
2024-03-27 21:19:40 +0000

    array done

---
+++ 2_basics/2_arrays.js
@@ -0,0 +1,16 @@
+let marvel = ["ironman","thor","marvel"];
+let dc = ["batman","superman","flash"];
+
+// marvel.push(dc);
+
+// console.log(marvel);
+// let all = marvel.concat(dc);
+// console.log(all);
+// spread operator
+// let all = [...dc, ...marvel];
+// console.log(all);
+// let array3 = [1,2,3,4,5,6,7,[8,9,[10]]];
+// let usableArray3 = array3.flat(Infinity);
+// console.log(usableArray3);
+
+
