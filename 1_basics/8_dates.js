f77cd1159d9323182058f35f9380a39a0520b78e
Shubham Relhan <shrelhan.relhan@gmail.com>
2024-03-27 20:33:07 +0000

    date done

---
+++ 1_basics/8_dates.js
@@ -0,0 +1,16 @@
+let myDate = new Date();
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString('en-IN'));
+
+let myBirthDate = new Date(1996, 2, 17)
+
+// console.log(myBirthDate.toLocaleDateString('en-IN'));
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+// console.log(myBirthDate.getTime());
+
+
+
